39b43b775b19f38dd351b2ded893205567407df8
Amiel Martin <amiel@actionsprout.com>
2015-01-18 19:43:53 -0800

    Setup ember-cli-cordova cordova-starter-kit

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,17 @@
 /* jshint node: true */
 
+var os     = require('os');
+var ifaces = os.networkInterfaces();
+
+var addresses = [];
+for (var dev in ifaces) {
+  ifaces[dev].forEach(function(details){
+    if(details.family === 'IPv4' && details.address !== '127.0.0.1') {
+      addresses.push(details.address);
+    }
+  });
+}
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'clicker-app',
@@ -20,7 +32,12 @@ module.exports = function(environment) {
 
     cordova: {
       rebuildOnChange: false,
-      emulate: false
+      emulate: false,
+      emberUrl: 'http://' + addresses[0] + ':4200',
+      liveReload: {
+        enabled: false,
+        platform: 'ios'
+      }
     }
   };
 
@@ -30,6 +47,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.apiUrl   = 'http://' + addresses[0] + ':3000/api/v1';
+    ENV.development = true;
   }
 
   if (environment === 'test') {
@@ -44,8 +64,15 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'staging') {
+    ENV.apiUrl = 'http://clicker-app-staging.herokuapp.com/api/v1';
+    ENV.staging = true;
+  }
 
+
+  if (environment === 'production') {
+    ENV.apiUrl = 'http://clicker-app.herokuapp.com/api/v1';
+    ENV.production = true;
   }
 
   return ENV;
