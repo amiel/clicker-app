70dfbdddc50f6a4855ec90c5ca99eaa3da023a1c
Amiel Martin <amiel@actionsprout.com>
2015-03-15 14:51:18 -0700

    Add ability to delete sessions

--- app/routes/session.js
+++ app/routes/session.js
@@ -5,5 +5,15 @@ import BackButtonRoute from 'clicker-app/mixins/back-button-route';
 export default Ember.Route.extend(BackButtonRoute, {
   model: function(params) {
     return this.store.find('session', params.session_id);
+  },
+
+
+  actions: {
+    destroy: function() {
+      if (window.confirm("Are you sure you want to delete this session?")) {
+        this.controller.get('model').destroyRecord();
+        this.send('back');
+      }
+    }
   }
 });
