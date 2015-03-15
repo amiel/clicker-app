a9f0c336ccad12634dcfa2b9821a80dae9b2af94
Amiel Martin <amiel@actionsprout.com>
2015-03-15 15:08:47 -0700

    Set up animations

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,45 @@
 export default function() {
   this.transition(
-    this.fromRoute('index'),
-    this.toRoute('start'),
+    this.fromRoute('sessions'),
+    this.toRoute('session'),
     this.use('toLeft'),
     this.reverse('toRight')
   );
+
+  this.transition(
+    this.fromRoute('device'),
+    this.toRoute('session'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+
+  this.transition(
+    this.fromRoute('sessions'),
+    this.toRoute('devices'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+
+  this.transition(
+    this.fromRoute('devices'),
+    this.toRoute('loading'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+
+  this.transition(
+    this.fromRoute('loading'),
+    this.toRoute('device'),
+    this.use('crossFade')
+  );
+
+  this.transition(
+    this.fromRoute('device'),
+    this.toRoute('sessions'),
+    this.use('crossFade')
+  );
+
 }
